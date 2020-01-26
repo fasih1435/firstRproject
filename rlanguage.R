52ac246af30be45ad6e1149486e212aa69cb332c
fasih1435 <f168171@nu.edu.pk>
2020-01-26 22:38:44 +0500

    Learning R language

--- rlanguage.R
+++ rlanguage.R
@@ -1,3 +1,22 @@
-a=5
-b=6
-c=6
+myString="Hello World"
+x<-1L:50
+print(x)
+print ( myString)
+# comment
+z<-vector("numeric",length = 10)
+z
+x
+y=1/Inf
+y
+0/0
+attributes(x)
+q<-c(1.7,"a")                                          
+q
+q<-c(F,T)
+q
+x<-0:100
+class(x)
+as.numeric(x) #Objects can be explicitly coerced from one class to another using the as.* functions
+as.character((x))
+as.logical(x)
+x
